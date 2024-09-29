b0868f4b11fd1c8621524d75097d8c43c10eb4bd
Francisco Gorina <fgorina@gmail.com>
2024-09-29 11:44:01 +0200

    Works listing devices and messages

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Connect the USB tpo the computer and it will report in ActiSenser text format the NMEA 2000 messages.
 
 Also will show some in the screen.
+
+When starting it does not do anything. Just emter a copmmand :
+
+t or T enable trace of messages
+esc disables tracing of messages
+l of L lists devices found in network
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,19 @@
 #include <Arduino.h>
-#include "m5StickCPlus2.h"
+#include "M5StickCPlus2.h"
 
 #define ESP32_CAN_TX_PIN GPIO_NUM_26
 #define ESP32_CAN_RX_PIN GPIO_NUM_36
 
 #include <NMEA2000.h>
-#include <NMEA2000_esp32.h> 
+#include <NMEA2000_esp32.h>
 #include <N2kMessages.h>
-// List here messages your device will transmit.
-const unsigned long TransmitMessages[] PROGMEM={127506L,127508L,127513L,0};
-
-// ---  Example of using PROGMEM to hold Product ID.  However, doing this will prevent any updating of
-//      these details outside of recompiling the program.
-const tNMEA2000::tProductInformation BatteryMonitorProductInformation PROGMEM={
-                                       2100,                        // N2kVersion
-                                       100,                         // Manufacturer's product code
-                                       "Simple battery monitor",    // Manufacturer's Model ID
-                                       "1.2.0.16 (2022-10-01)",     // Manufacturer's Software version code
-                                       "1.2.0.0 (2022-10-01)",      // Manufacturer's Model version
-                                       "00000001",                  // Manufacturer's Model serial code
-                                       0,                           // SertificationLevel
-                                       1                            // LoadEquivalency
-                                      };                                      
+#include "N2kDeviceList.h"
 
 // ---  Example of using PROGMEM to hold Configuration information.  However, doing this will prevent any updating of
 //      these details outside of recompiling the program.
-const char BatteryMonitorManufacturerInformation [] PROGMEM = "John Doe, john.doe@unknown.com"; 
-const char BatteryMonitorInstallationDescription1 [] PROGMEM = "Just for sample"; 
-const char BatteryMonitorInstallationDescription2 [] PROGMEM = "No real information send to bus"; 
+const char BatteryMonitorManufacturerInformation[] PROGMEM = "John Doe, john.doe@unknown.com";
+const char BatteryMonitorInstallationDescription1[] PROGMEM = "Just for sample";
+const char BatteryMonitorInstallationDescription2[] PROGMEM = "No real information send to bus";
 
 // Define schedulers for messages. Define schedulers here disabled. Schedulers will be enabled
 // on OnN2kOpen so they will be synchronized with system.
@@ -35,78 +21,207 @@ const char BatteryMonitorInstallationDescription2 [] PROGMEM = "No real informat
 // Setup periods according PGN definition (see comments on IsDefaultSingleFrameMessage and
 // IsDefaultFastPacketMessage) and message first start offsets. Use a bit different offset for
 // each message so they will not be sent at same time.
-tN2kSyncScheduler DCBatStatusScheduler(false,1500,500);
-tN2kSyncScheduler DCStatusScheduler(false,1500,510);
-tN2kSyncScheduler BatConfScheduler(false,5000,520); // Non periodic
+tN2kSyncScheduler DCBatStatusScheduler(false, 1500, 500);
+tN2kSyncScheduler DCStatusScheduler(false, 1500, 510);
+tN2kSyncScheduler BatConfScheduler(false, 5000, 520); // Non periodic
 
 tNMEA2000_esp32 NMEA2000;
 
+tN2kDeviceList *pN2kDeviceList;
 
 void HandleNMEA2000Msg(const tN2kMsg &N2kMsg);
 
-void setup() {
+//*****************************************************************************
+void PrintUlongList(const char *prefix, const unsigned long *List)
+{
+  uint8_t i;
+  if (List != 0)
+  {
+    Serial.print(prefix);
+    for (i = 0; List[i] != 0; i++)
+    {
+      if (i > 0)
+        Serial.print(", ");
+      Serial.print(List[i]);
+    }
+    Serial.println();
+  }
+}
 
-    // M5 Setup screen 
+//*****************************************************************************
+void PrintText(const char *Text, bool AddLineFeed = true)
+{
+  if (Text != 0)
+    Serial.print(Text);
+  if (AddLineFeed)
+    Serial.println();
+}
 
-    auto cfg = M5.config();
-    StickCP2.begin(cfg);
-    StickCP2.Display.setRotation(1);
-    StickCP2.Display.setTextColor(GREEN);
-    StickCP2.Display.setTextDatum(middle_center);
-    StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
-    StickCP2.Display.setTextSize(1);
-    // NMEA2000 Config
+//*****************************************************************************
+void PrintDevice(const tNMEA2000::tDevice *pDevice)
+{
+  if (pDevice == 0)
+    return;
 
+  Serial.println("----------------------------------------------------------------------");
+  Serial.println(pDevice->GetModelID());
+  Serial.print("  Source: ");
+  Serial.println(pDevice->GetSource());
+  Serial.print("  Manufacturer code:        ");
+  Serial.println(pDevice->GetManufacturerCode());
+  Serial.print("  Unique number:            ");
+  Serial.println(pDevice->GetUniqueNumber());
+  Serial.print("  Software version:         ");
+  Serial.println(pDevice->GetSwCode());
+  Serial.print("  Model version:            ");
+  Serial.println(pDevice->GetModelVersion());
+  Serial.print("  Manufacturer Information: ");
+  PrintText(pDevice->GetManufacturerInformation());
+  Serial.print("  Installation description1: ");
+  PrintText(pDevice->GetInstallationDescription1());
+  Serial.print("  Installation description2: ");
+  PrintText(pDevice->GetInstallationDescription2());
+  PrintUlongList("  Transmit PGNs :", pDevice->GetTransmitPGNs());
+  PrintUlongList("  Receive PGNs  :", pDevice->GetReceivePGNs());
+  Serial.println();
+}
 
+#define START_DELAY_IN_S 8
+//*****************************************************************************
+void ListDevices(bool force = false)
+{
+  static bool StartDelayDone = false;
+  static int StartDelayCount = 0;
+  static unsigned long NextStartDelay = 0;
+  if (!StartDelayDone)
+  { // We let system first collect data to avoid printing all changes
+    if (millis() > NextStartDelay)
+    {
+      if (StartDelayCount == 0)
+      {
+        Serial.print("Reading device information from bus ");
+        NextStartDelay = millis();
+      }
+      Serial.print(".");
+      NextStartDelay += 1000;
+      StartDelayCount++;
+      if (StartDelayCount > START_DELAY_IN_S)
+      {
+        StartDelayDone = true;
+        Serial.println();
+      }
+    }
+    return;
+  }
+  if (!force && !pN2kDeviceList->ReadResetIsListUpdated())
+    return;
 
-                                
-  // Uncomment 3 rows below to see, what device will send to bus                           
+  Serial.println();
+  Serial.print("Max devices ");
+  Serial.println(N2kMaxBusDevices);
+  Serial.println("**********************************************************************");
+  for (uint8_t i = 0; i < N2kMaxBusDevices; i++)
+  {
+
+    PrintDevice(pN2kDeviceList->FindDeviceBySource(i));
+
+  }
+}
+
+void setup()
+{
+
+  // M5 Setup screen
+
+  auto cfg = M5.config();
+  StickCP2.begin(cfg);
+  StickCP2.Display.setRotation(1);
+  StickCP2.Display.setTextColor(GREEN);
+  StickCP2.Display.setTextDatum(middle_center);
+  StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
+  StickCP2.Display.setTextSize(1);
+  // NMEA2000 Config
+
+  NMEA2000.SetProductInformation("00000003",                // Manufacturer's Model serial code
+                                 100,                       // Manufacturer's product code
+                                 "N2k bus device analyzer", // Manufacturer's Model ID
+                                 "1.0.0.10 (2017-07-29)",   // Manufacturer's Software version code
+                                 "1.0.0.0 (2017-07-12)"     // Manufacturer's Model version
+  );
+
+  NMEA2000.SetDeviceInformation(2,   // Unique number. Use e.g. Serial number.
+                                130, // Device function=Display. See codes on https://web.archive.org/web/20190531120557/https://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+                                120, // Device class=Display. See codes on  https://web.archive.org/web/20190531120557/https://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+                                2046 // Just choosen free from code list on https://web.archive.org/web/20190529161431/http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+  );
+
+  // Uncomment 3 rows below to see, what device will send to bus
   Serial.begin(115200);
   NMEA2000.SetForwardStream(&Serial);
   // NMEA2000.SetForwardType(tNMEA2000::fwdt_Text);     // Show in clear text. Leave uncommented for default Actisense format.
- // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
-  
-  NMEA2000.SetForwardType(tNMEA2000::fwdt_Text);     // Uncomment this, so you can test code without CAN bus chips on Arduino Mega
-  // NMEA2000.EnableForward(false);                      // Disable all msg forwarding to USB (=Serial)
-  
-//  NMEA2000.SetN2kCANMsgBufSize(2);                    // For this simple example, limit buffer size to 2, since we are only sending data
-  // Define OnOpen call back. This will be called, when CAN is open and system starts address claiming.
-  NMEA2000.SetMsgHandler(HandleNMEA2000Msg);
-  NMEA2000.Open();
+  // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
 
+  NMEA2000.SetForwardType(tNMEA2000::fwdt_Text); // Uncomment this, so you can test code without CAN bus chips on Arduino Mega
+                                                 // NMEA2000.SetDebugMode(tNMEA2000::dm_Actisense);
+  NMEA2000.EnableForward(false);                 // Disable all msg forwarding to USB (=Serial)
+  NMEA2000.SetMode(tNMEA2000::N2km_ListenAndNode, 50);
+
+  //  NMEA2000.SetN2kCANMsgBufSize(2);                    // For this simple example, limit buffer size to 2, since we are only sending data
+  // Define OnOpen call back. This will be called, when CAN is open and system starts address claiming.
+  pN2kDeviceList = new tN2kDeviceList(&NMEA2000);
+  // NMEA2000.SetMsgHandler(HandleNMEA2000Msg);
+  NMEA2000.Open();
 }
 
 int delta = 0;
-void loop() {
-    NMEA2000.ParseMessages();
-    delta++;
-    if(delta >= 10000){  
-      Serial.print("*");
-      delta = 0;
+void loop()
+{
+  NMEA2000.ParseMessages();
+  ListDevices();
+
+  if (Serial.available() > 0)
+  {
+    int command = Serial.read();
+    Serial.print("Received command : ");
+    Serial.println(command);
+
+    if (command == 108 || command == 76)
+    {
+      ListDevices(true);
+    }
+    else if (command == 84 || command == 116)
+    {
+      NMEA2000.EnableForward(true);
+    }
+    else if (command == 27)
+    {
+      NMEA2000.EnableForward(false);
+    }
   }
 }
-  
-void HandleNMEA2000Msg(const tN2kMsg &N2kMsg) {
+
+void HandleNMEA2000Msg(const tN2kMsg &N2kMsg)
+{
   unsigned char SID;
   unsigned char instance;
   double voltage;
-  
-  
-  
-    double current;
+
+  double current;
   double temperature;
 
-
-  if (N2kMsg.PGN == 127508){
-    if(ParseN2kDCBatStatus(N2kMsg, instance, voltage, current, temperature, SID)){
+  if (N2kMsg.PGN == 127508)
+  {
+    if (ParseN2kDCBatStatus(N2kMsg, instance, voltage, current, temperature, SID))
+    {
       StickCP2.Display.clear();
-        StickCP2.Display.setCursor(10, 30);
-        int vol = floor(voltage * 1000);
-        StickCP2.Display.printf("BAT: %dmv", vol);
-    }else{
-      Serial.println("Malformed message");
+      StickCP2.Display.setCursor(10, 30);
+      int vol = floor(voltage * 1000);
+      StickCP2.Display.printf("BAT: %dmv", vol);
     }
-  }else{
-    Serial.print("Rebut PGN "); Serial.println(N2kMsg.PGN);
+  }
+  else
+  {
+    Serial.print("Rebut PGN ");
+    Serial.println(N2kMsg.PGN);
   }
 }
